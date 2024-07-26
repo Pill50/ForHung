911d18bc442ad00737be11746eb4eb9b326d292b
vinhphuctran <vinhphuccse@gmail.com>
2024-07-26 18:21:10 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Loading from './components/Loading';
+import AuthLayout from './layouts/AuthLayout';
+import MainLayout from './layouts/MainLayout';
+import { ConfirmOtp, IdeaInspirePage, InspirePage, Login, ScratchDetailPage, ScratchPage } from './routes';
+import ProtectedRoute from './routes/ProtectedRoute';
+import { NotFound, Profile, Service } from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <React.Suspense fallback={<Loading />}>
+        <Routes>
+          <Route element={<AuthLayout />}>
+            <Route path={'/login'} element={<Login />}></Route>
+            <Route path={'/confirm-otp'} element={<ConfirmOtp />}></Route>
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route element={<MainLayout />}>
+              <Route path={'/services'}>
+                <Route index element={<Service />}></Route>
+                <Route path='scratch'>
+                  <Route index element={<ScratchPage />}></Route>
+                  <Route path=':detail' element={<ScratchDetailPage />}></Route>
+                </Route>
+                <Route path='inspire'>
+                  <Route index element={<InspirePage />}></Route>
+                  <Route path='idea' element={<IdeaInspirePage />}></Route>
+                </Route>
+              </Route>
+              <Route path={'/profile'}>
+                <Route index element={<Profile />}></Route>
+              </Route>
+            </Route>
+          </Route>
+          <Route path={'*'} element={<NotFound />}></Route>
+        </Routes>
+      </React.Suspense>
+    </>
   );
 }
 
